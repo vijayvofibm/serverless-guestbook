80f647d91121a8cb664828749f00a900d0427b43
Vijay Venkataramani <vvenkata@us.ibm.com>
2020-08-11 10:33:06 -0700

    Update guestbook.js
    Modified the apiURL to mine https://100d7b8c.us-south.apigw.appdomain.cloud/guestbook

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://100d7b8c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
